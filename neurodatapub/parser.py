fd58d7130e83eece33a3f9f3c88d7b3687912e17
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2021-07-28 11:24:09 +0200

    ENH: Improve `parser.py`

--- neurodatapub/parser.py
+++ neurodatapub/parser.py
@@ -6,49 +6,49 @@
 
 """NeuroDataPub Commandline Parser."""
 
+import sys
+import argparse
 from neurodatapub.info import __version__
 from neurodatapub.info import __release_date__
 
 
 def get_parser():
     """Create and return the parser object of NeuroDataPub."""
-
-    import argparse
-
-    p = argparse.ArgumentParser(description="Argument parser of NeuroDataPub")
-
-    p.add_argument("--bids_dir", help="The directory with the input dataset "
-                                      "formatted according to the BIDS standard.")
-
-    p.add_argument(
-        "--output_datalad_dir",
-        help="The local directory where the output Datalad "
-        "dataset should be stored.",
+    p = argparse.ArgumentParser(
+        description=f"Argument parser of NeuroDataPub ({__version__})"
     )
 
+    p.add_argument(
+        "--bids_dir",
+        help="The directory with the input dataset "
+             "formatted according to the BIDS standard.",
+        required=True
+    )
+    p.add_argument(
+        "--datalad_dir",
+        help="The local directory where the Datalad dataset should be.",
+        required=True
+    )
     p.add_argument(
         "--git-annex-ssh-special-sibling-config",
         help="Path to a JSON file containing configuration "
              "parameters for the git-annex SSH special remote dataset sibling",
-        default=None,
-        type=str,
+        required='--gui' not in " ".join(sys.argv),
+        type=str
     )
-
     p.add_argument(
-            "--github-sibling-config",
-            help="Path to a JSON file containing configuration "
-                 "parameters for the GitHub dataset repository sibling",
-            default=None,
-            type=str,
+        "--github-sibling-config",
+        help="Path to a JSON file containing configuration "
+             "parameters for the GitHub dataset repository sibling",
+        required='--gui' not in " ".join(sys.argv),
+        type=str
     )
-
     p.add_argument(
         "--gui",
         help="Run NeuroDataPub in GUI mode",
         action="store_true",
         default=False,
     )
-
     p.add_argument(
         "-v",
         "--version",

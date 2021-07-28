3590625c730055c40aebc8811b04488555c1fda4
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2021-07-28 12:03:21 +0200

    FIX: Correct __version__ and review code style in `info.py`

--- neurodatapub/info.py
+++ neurodatapub/info.py
@@ -5,16 +5,16 @@ _version_minor = 1
 
 __release_date__ = 'DD.MM.2021'
 
-__version__ = "%s.%s.%s%s" % (_version_major,
-                              _version_minor)
+__version__ = "%s.%s" % (_version_major, _version_minor)
 
 # __current_year__ = datetime.datetime.now().strftime("%Y")
 __current_year__ = '2021'
 
 __author__ = 'Sebastien Tourbier'
-__copyright__ = 'Copyright {}, Connectomics Lab, Lausanne and contributors'.format(__current_year__)
-__credits__ = ('Contributors: please check the ``.zenodo.json`` file at the top-level folder'
-               'of the repository')
+__copyright__ = ('Copyright {year}, '.format(year=__current_year__)
+                 + 'Connectomics Lab, Lausanne and contributors')
+__credits__ = ('Contributors: please check the ``.zenodo.json`` file '
+               'at the top-level folder of the repository')
 __license__ = 'Apache 2.0'
 __maintainer__ = 'Sebastien Tourbier'
 __email__ = 'sebastien.tourbier@alumni.epfl.ch'
@@ -26,4 +26,5 @@ __url__ = 'https://github.com/NCCR-SYNAPSY/{name}'.format(name=__packagename__)
 
 DOWNLOAD_URL = (
     'https://github.com/NCCR-SYNAPSY/{name}/archive/{ver}.tar.gz'.format(
-        name=__packagename__, ver=__version__))
+        name=__packagename__, ver=__version__)
+)

cebebe9a2b2c5185957442eb31d97d29b4d3dc5b
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2021-07-30 16:02:37 +0200

    DEBUG: Review how to reurn the style sheet for save json button

--- neurodatapub/utils/qt.py
+++ neurodatapub/utils/qt.py
@@ -185,4 +185,4 @@ def return_save_json_button_style_sheet():
             min-width: 20px;
         }
         '''
-    return style_sheet_save_json_button.format(save_json_icon)
+    return style_sheet_save_json_button

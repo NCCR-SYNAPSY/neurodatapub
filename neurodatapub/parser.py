2fd29337c8fd76c7c4ffa2bbb1617c8442f8d1d4
Sebastien Tourbier <sebastien.tourbier1@gmail.com>
2021-08-03 18:59:57 +0200

    DOC: Update description of `parser.py`

--- neurodatapub/parser.py
+++ neurodatapub/parser.py
@@ -15,15 +15,15 @@ from neurodatapub.info import __release_date__
 def get_parser():
     """Create and return the parser object of NeuroDataPub."""
     p = argparse.ArgumentParser(
-        description=f"Argument parser of NeuroDataPub ({__version__})"
+        description=f"Command-line argument parser of `NeuroDataPub` (v{__version__})"
     )
 
     p.add_argument(
         "--mode",
-        help="Mode in which `neurodatapub` is run: "
-             '`"create-only"` creates the datalad dataset only, '
-             '`"publish-only"` creates the datalad dataset only, '
-             '`"all"` creates the datalad dataset only, ',
+        help="Mode in which ``neurodatapub`` is run: "
+             '``"create-only"`` creates the datalad dataset only, '
+             '``"publish-only"`` creates the datalad dataset only, '
+             '``"all"`` creates the datalad dataset only, ',
         choices=["all", "create-only", "publish-only"],
         required='--gui' not in " ".join(sys.argv),
         type=str
@@ -63,6 +63,6 @@ def get_parser():
         "-v",
         "--version",
         action="version",
-        version=f"NeuroDataPub version {__version__} (Released: {__release_date__})",
+        version=f"``neurodatapub`` version {__version__} (Released: {__release_date__})",
     )
     return p
